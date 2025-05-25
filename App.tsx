91a70bd785dce0ccca71db8520c23f8d17dea018
Kaushal_Jha_Mac <kushal_jha_mac@Kaushals-Mac.local>
2025-05-26 00:13:48 +0530

    __

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-import { Text } from "react-native";
-import Fonts from "@constants/Fonts";
-import COLORS from "@constants/Color";
 import SplashScreen from "@screens/SplashScreen/SplashScreen";
 
 export default function App() {
